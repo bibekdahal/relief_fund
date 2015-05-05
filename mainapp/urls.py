78c6b295fd9d90a9196859453621092a47680178
NISH1001 <nish_play1001@yahoo.com>
2015-05-05 12:28:42 +0545

    basic backbone

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'relief_fund.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.Index.as_view(), name="index"),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^mainapp/', include('mainapp.urls', namespace='mainapp')),
-    url(r'^$', include('mainapp.urls', namespace='mainapp')),
 )
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,2 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from mainapp.models import *
+
+class Index(View):
+
+    def get(self,request):
+        context = {}
+        return render(request, 'index.html', context)
 
