759cb45b8b328e3026394596c7260319b6439817
NISH1001 <nish_play1001@yahoo.com>
2015-05-05 17:35:12 +0545

    form processed

--- mainapp/views.py
+++ mainapp/views.py
@@ -28,6 +28,8 @@ class FundView(View):
         provider_name = request.POST.get('provider_name')
         provider_phone = request.POST.get('provider_phone')
         provider = Provider.objects.filter(name=provider_name, phone=provider_phone)
+        if provider:
+            provider = provider[0]
         if not provider:
             provider = Provider(name=provider_name, phone=provider_phone)
             provider.save()
@@ -37,11 +39,25 @@ class FundView(View):
         latitude = request.POST.get('latitude')
         longitude = request.POST.get('longitude')
         place = Place.objects.filter(name=place_name)
+        if place:
+            place = place[0]
         if not place:
             place = Place(name=place_name, district=district_name, latitude=latitude, longitude=longitude)
             place.save()
 
+        fundstate = int(request.POST.get("fundstate"))
+        fund = Fund(provider=provider, place=place, state=fundstate)
+        fund.save()
+
         itemtype_list = ItemType.objects.all()
+        for itemtype in itemtype_list:
+            amount = int(request.POST.get(itemtype.name+"_amount"))
+            if amount>0:
+                item = Item(type=itemtype, amount=amount, fund=fund)
 
         return HttpResponseRedirect(reverse('mainapp:fund'))
+
+
+
+
 
