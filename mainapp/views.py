0fb502f0cbc05bd2779e976dde0459696d312b60
NISH1001 <nish_play1001@yahoo.com>
2015-05-05 15:15:31 +0545

    provider and place form

--- mainapp/views.py
+++ mainapp/views.py
@@ -12,3 +12,29 @@ class Index(View):
         context = {}
         return render(request, 'index.html', context)
 
+class Fund(View):
+
+    def get(self, request):
+        context = {}
+        return render(request, 'fund.html', context)
+
+    def post(self, request):
+        context = {}
+        provider_name = request.POST.get('provider_name')
+        provider_phone = request.POST.get('provider_phone')
+        provider = Provider.objects.filter(name=provider_name, phone=provider_phone)
+        if not provider:
+            provider = Provider(name=provider_name, phone=provider_phone)
+            provider.save()
+
+        place_name = request.POST.get('place')
+        district_name = request.POST.get('district')
+        latitude = request.POST.get('latitude')
+        longitude = request.POST.get('longitude')
+        place = Place.objects.filter(name=place_name)
+        if not place:
+            place = Place(name=place_name, district=district_name, latitude=latitude, longitude=longitude)
+            place.save()
+
+        return HttpResponseRedirect(reverse('mainapp:fund'))
+
